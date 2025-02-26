433848753fd695b9eee0bdde23ea79f118e938b7
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2025-02-26 11:11:29 +0100

    feat: Update main function to utilize Anthill and Chamber classes

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
-#include "Headers/Coucou.hpp"
+#include "Headers/Anthill.hpp"
 using namespace std;
 
 int main() {
-    Coucou coucou;
-    coucou.hello();
+    Anthill anthill(4);
+
+    anthill.chambers[0] = Chamber("Chamber Sv", 5);
+    anthill.chambers[1] = Chamber("Chamber 1", 10);
+    anthill.chambers[2] = Chamber("Chamber 2", 3);
+    anthill.chambers[3] = Chamber("Chamber Sd", 8);
+
+    anthill.addTunnel(0, 1);
+    anthill.addTunnel(0, 2);
+    anthill.addTunnel(1, 3);
+    anthill.addTunnel(2, 3);
+
+    anthill.printAnthill();
+    anthill.printNumberOfAnts();
+
     return 0;
 }
