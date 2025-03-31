242b9c9e2ddcd5305e17e81dc119014054c78611
baptiste-appriou <baptiste.appriou@laplateforme.io>
2025-03-31 09:30:09 +0200

    [refactor] Improve code readability in Chamber class by enhancing formatting and adding comments

--- Sources/Chamber.cpp
+++ Sources/Chamber.cpp
@@ -1,44 +1,59 @@
 #include "Headers/Chamber.hpp"
 
-Chamber::Chamber(string name, int maxAnts) {
+Chamber::Chamber(string name, int maxAnts)
+{
     this->name = name;
     this->maxAnts = maxAnts;
 }
 
 Chamber::~Chamber() {}
 
-
-void Chamber::removeAnt() {
-    if (currentAnts > 0) {
+// Remove an ant from the chamber
+void Chamber::removeAnt()
+{
+    if (currentAnts > 0)
+    {
         currentAnts--;
-    } else {
+    }
+    else
+    {
         cout << "Chamber " << name << " is empty." << endl;
     }
 }
 
-int Chamber::getCurrentAnts() {
+// Getters for current and max ants, and chamber name
+int Chamber::getCurrentAnts()
+{
     return currentAnts;
 }
 
-int Chamber::getMaxAnts() const {
+int Chamber::getMaxAnts() const
+{
     return maxAnts;
 }
 
-string Chamber::getName() const {
+string Chamber::getName() const
+{
     return name;
 }
 
-void Chamber::addAnts(int ants) {
+// Add ants to the chamber, respecting the maximum capacity
+void Chamber::addAnts(int ants)
+{
     // Allow infinite ants for Sv and Sd
-    if (maxAnts == -1) {
+    if (maxAnts == -1)
+    {
         currentAnts += ants;
         return;
     }
 
     // Regular chamber logic
-    if (currentAnts + ants <= maxAnts) {
+    if (currentAnts + ants <= maxAnts)
+    {
         currentAnts += ants;
-    } else {
+    }
+    else
+    {
         cout << "Chamber " << name << " is full." << endl;
     }
 }
